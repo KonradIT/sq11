b02e1f2bc212ccef8c1ad8924297900f4494e18c
konradit <github@chernowii.com>
2017-03-24 22:25:41 +0100

    numbers ...

---
+++ numb.py
@@ -0,0 +1,13 @@
+## Proof of concept -- Run this on a supercomputer -- it *will* crash
+## Flags:
+debug=True
+print_for_x=True
+i = 1
+while str(i**2)[-2:] != '11':
+	x = str(i**2)[:-2]
+	if debug == True:
+		print("i int: " + str(i))
+	if print_for_x == True:    
+		print("x int: " + str(x))
+	i += 1
+print ("magical number; " + str(i) + ' -- ' + str(i**2))
